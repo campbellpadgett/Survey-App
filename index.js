64daa00bf5be2cf1c6687ab5456b1d5f63f374e8
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-02-19 14:47:55 -0600

    Passport added

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 
 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there, friend' })
-});
+passport.use(new GoogleStrategy());
  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
