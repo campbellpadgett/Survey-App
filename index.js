d56ff2636b2799570397c5d67a83ce34ca8ef4c0
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-02-23 14:47:18 -0600

    installing mongoose.js
    -restructured server
    -installed mongoose.js
    connected to atlas mongoDB

--- index.js
+++ index.js
@@ -3,33 +3,14 @@
 
 
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
+const mongoose = require('mongoose')
+require('./services/passport')
 
-const app = express();
+mongoose.connect(keys.mongoURI)
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken', accessToken)
-            console.log('refreshToken', refreshToken)
-            console.log('profile', profile)
-            console.log('done', done)
-        }
-    ) 
-);
+const app = express()
+require('./routes/authRoutes')(app)
 
-app.get('/auth/google', passport.authenticate('google', { 
-    scope: ['profile', 'email']
-}))
 
-app.get('/auth/google/callback', passport.authenticate('google'))
- 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
