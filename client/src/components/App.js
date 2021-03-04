baf271445235faf29eb9337a564ef3db4568abcc
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-04 11:14:57 -0600

    added react-router routes
    - removed h1 and link
    
    + Header.js
    + Dashboard.js
    + SurveyNew.js
    + Landing.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,29 @@
+
+
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './Header'
+import Dashboard from './Dashboard'
+import SurveyNew from './SurveyNew'
+import Landing from './Landing'
+
+
 const App = () => {
   return (
     <div className='ui container'>
-      <h1>Hello World</h1>
-      <a href="/auth/google">Sign into Google</a>
+
+      <BrowserRouter>
+        <div>
+          <Route path='/' component={Header}  />
+          <Route path='/' component={Landing}  />        
+          <Route path='/surveys' component={Dashboard}  />
+          <Route path='/surveys/new' component={SurveyNew}  />
+        </div>
+      </BrowserRouter>
+
     </div>
   );
 }
 
 export default App;
+ 
