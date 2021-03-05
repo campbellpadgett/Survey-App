4b2de9d4cb31c9b737c585ef01657071e075c7b2
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-05 12:25:00 -0600

    Updated Header
    + renderContent()
    - header is now responsive to user being signed in or not

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-const Header = () => {
+const Header = props => {
+
+    const renderContent = () => {
+        switch(props.auth) {
+            case null:
+                return "working on it..."
+            case false:
+                return 'Sign Into Google'
+            default:
+                return 'Log Out'
+        }
+    }
+
     return (
         <div>
             <nav>
                 <div class="nav-wrapper">
                     <a href="/" class="center brand-logo">Emaily</a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="/auth/google">Login With Google</a></li>
+                        <li><a href="/auth/google">{renderContent()}</a></li>
                     </ul>
                 </div>
             </nav>
@@ -15,4 +28,8 @@ const Header = () => {
     );
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+    return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
