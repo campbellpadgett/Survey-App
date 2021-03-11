317cb05b5414d69114a2cd23ba2a861086f85f9b
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-11 10:33:48 -0600

    added middleware to check for login

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,9 +3,10 @@
 
 const keys = require('../config/keys')
 const stripe = require("stripe")(keys.stripeSecretKey)
+const requireLogin = require('../middleware/requireLogin')
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create(
             {
                 amount: 500,
@@ -14,5 +15,11 @@ module.exports = app => {
                 source: req.body.id
             }
         );
+
+        req.user.credits += 5
+        const user = await req.user.save()
+        console.log(user)
+
+        res.send(user)
     })
 }
