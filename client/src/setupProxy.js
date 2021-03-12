41dc7c0be388e5cb88ec7779f7b5035ea38008aa
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-12 10:44:36 -0600

    test 4

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/api/current_user", "/auth/google"],
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://calm-cove-98603.herokuapp.com",
     })
   );
 };
