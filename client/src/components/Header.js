c4312b55a9a434e0243effdb9015113fcc290225
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-12 09:54:14 -0600

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,11 +8,7 @@ const Header = props => {
     const renderContent = () => {
         switch(props.auth) {
             case null:
-                return [
-                <li key={Math.random()}><Payments /></li>,  
-                <li key={Math.random()} style={{ margin: '0 10px' }}>Credits: {props.auth.credits}</li>,
-                <li key={Math.random()}><a href='/api/logout'>Log Out</a></li>
-            ]
+                return 
             case false:
                 return <a href="/auth/google">Sign Into Google</a>
             default:
