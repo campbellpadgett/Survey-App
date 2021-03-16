8fe0b224157d29e2e1aec31ec83af418f579ddc8
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-16 16:17:17 -0500

    added to /api/surveys route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,10 +1,34 @@
+const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const requireCredits = require('../middleware/requireCredits')
+
+
+const Survey = mongoose.model('surveys')
+
 
 module.exports = app => {
 
     app.get('/api/surveys', requireLogin, requireCredits, (req, res) => {
-        res.send('list and creation of surveys')
+
+
+
+
+        const { title, subject, body, recipients } = req.body
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        })
+
+        survey.save()
+
+
+
+
+
     })
 
     app.post('/api/surveys/webhooks', requireLogin, (req, res) => {
