c3b0bf7bd6a907e381b1667e5e75d822f3be490c
campbellpadgett <48736292+campbellpadgett@users.noreply.github.com>
2021-03-16 13:25:59 -0500

    created recipientSchema

--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const recipientSchema = require('./Recipient')
 
 const surveySchema = new Schema({
     title: String,
     subject: String,
     body: String,
-    recipients: [String],
-    yes: Number,
-    no: Number
+    recipients: [recipientSchema],
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 }
 });
 
 mongoose.model('surveys', surveySchema);
